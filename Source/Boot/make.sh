b4cdee7bf252ae0a6e8c8a561a0b586bb6ea3010
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2023-12-12 09:13:40 -0300

    Update make.sh

--- Source/Boot/make.sh
+++ Source/Boot/make.sh
@@ -1,4 +1,4 @@
 nasm -fbin Bootloader/boot.asm -o Binaries/boot.bin
 nasm -fbin Kernel/kernel.asm -o Binaries/kernel.bin
 
-cat Binaries/boot.bin Binaries/kernel.bin > Purgatorium.bin
+cat Binaries/boot.bin Binaries/kernel.bin > Infernum.bin
